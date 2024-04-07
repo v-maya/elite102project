2da2843ef24f3b913293517c187fd4c618a12b11
Maya <villa.maya26@gmail.com>
2024-04-07 17:24:59 -0500

    made fully functioning create_account() and log_in() functions. still working on modify_account() and delete_account()

--- information.py
+++ information.py
@@ -1,7 +1,7 @@
-# Testing of MySQL and experimenting, not yet adapted to needs
+#all python functions for main
 
 import mysql.connector
-from datetime import datetime
+#from datetime import datetime
 
 connection = mysql.connector.connect(
     host="localhost",
@@ -10,6 +10,9 @@ connection = mysql.connector.connect(
     database="banking"
     )
 
+# Allows the user to create an account saved onto an SQL table. IDs correspond with each user
+# and is used for identification.
+# This function is currently working.
 def create_account():
     cursor = connection.cursor()
     name = input("Enter your name: ")
@@ -18,14 +21,30 @@ def create_account():
     email = input("Enter your email: ")
     values = (name, username, password, email)
     add_data = ("INSERT INTO user_table (name, username, password, email) VALUES (%s, %s, %s, %s)")
-
     cursor.execute(add_data, values)
     connection.commit()
-
-    print(cursor.rowcount, "record inserted.")
-
+    #print(cursor.rowcount, "record inserted.")
     cursor.close()
     connection.close()
+
+# User types in their username and password and the function retrieves the corresponding ID to be used
+# in checking and making transactions.
+# This function is currently working. Returns the user ID as an integer.
+def log_in():
+    cursor = connection.cursor(buffered=True)
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+    values = (username, password)
+    sql = ("SELECT ID FROM user_table WHERE username=%s AND password=%s")
+    cursor.execute(sql, values)
+    connection.commit()
+    myresult = cursor.fetchone()
+    for x in myresult:
+        if isinstance(x, int):
+            return int(x)
+    cursor.close()
+    connection.close()
+    return myresult
 
 def check_balance():
     pass
@@ -36,9 +55,35 @@ def deposit():
 def withdraw():
     pass
 
-def modify_account():
-    pass
+# User enters in new data to update their existing account. User ID is needed to verify identity.
+# This function is currently not working.
+def modify_account(user_id):
+    cursor = connection.cursor()
+    name = input("Enter new name: ")
+    username = input("Create new username: ")
+    password = input("Create new password: ")
+    email = input("Enter your new email: ")
+    values = (name, username, password, email)
+    add_data = ("UPDATE user_table SET name = %s, username = %s, password = %s, email = %s WHERE ID = 4")
+    cursor.execute(add_data, values)
+    connection.commit()
+    print(cursor.rowcount, "record changed.")
+    cursor.close()
+    connection.close()
 
-def delete_account():
-    pass
+# Once logged in, the user will be able to delete their own account. However, the function should also work
+# if the user chooses to log in upon start up.
+# This function is currently not working.
+def delete_account(user_id):
+    if user_id == -1:
+        print("Whoops! Please log in first")
+        user_id = log_in()
+    cursor = connection.cursor()
+    value = (str(user_id))
+    remove_data = ("DELETE FROM user_table WHERE id=%s")
+    cursor.execute(remove_data, value)
+    connection.commit()
+    print(cursor.rowcount, "record deleted.")
+    cursor.close()
+    connection.close()
 
--- main.py
+++ main.py
@@ -6,23 +6,30 @@ main.py for test cases
 information.py for data handling
 '''
 import information as info
+
+# This ID verifies who is logged in and who can access what data. Set to -1 on start to avoid accessing accounts
+# without logging in.
+log_in_ID = -1
+
 # Display and select action
 def menu():
     print("\
            ----- Menu -----\n\
            1. Create account\n\
-           2. Check balance\n\
-           3. Deposit\n\
-           4. Withdraw\n\
-           5. Modify account\n\
-           6. Delete account\n\
+           2. Log in\n\
+           3. Check balance\n\
+           4. Deposit\n\
+           5. Withdraw\n\
+           6. Modify account\n\
+           7. Delete account\n\
           ")
     option = int(input("What would you like to do? "))
     match option:
         case 1:
             info.create_account()
         case 2:
-            print("Check balance")
+            global log_in_ID
+            log_in_ID = info.log_in()
         case 3:
             print("Deposit")
         case 4:
@@ -30,13 +37,17 @@ def menu():
         case 5:
             print("Modify account")
         case 6:
-            print("Delete account")
+            info.modify_account(log_in_ID)
+        case 7:
+            info.delete_account(log_in_ID)
         case _:
             print("Whoops!")
 
 # Welcome message for users
 def welcome():
     print("Hello! Welcome to the banking app.")
-    menu()
+    while True:
+        menu()
 
 welcome()
+print(log_in_ID)
