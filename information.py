2ee68b789736ce1580299cfe8c58d769a651d693
Maya <villa.maya26@gmail.com>
2024-04-28 23:32:06 -0500

    Updated information.py to have better function descriptions and removed tkinter functionality.

--- information.py
+++ information.py
@@ -1,7 +1,7 @@
 #all python functions for main
 
 import mysql.connector
-import tkinter as tk
+#import tkinter as tk
 
 connection = mysql.connector.connect(
     host="localhost",
@@ -30,6 +30,8 @@ def create_account():
 # in checking and making transactions.
 # This function is currently working. Returns the user ID as a list.
 def log_in():
+    """
+    The following tkinter code is commented out since it did not function as intended.
 
     def btn_func():
         cursor = connection.cursor(buffered=True)
@@ -65,40 +67,41 @@ def log_in():
     submit.pack()
     status_update = tk.Label(master=login_window, text="Logging in...")
     status_update.pack()
+    """
 
-    #cursor = connection.cursor(buffered=True)
-    #username = input("Enter your username: ")
-    #password = input("Enter your password: ")
-    #values = (username, password)
+    cursor = connection.cursor(buffered=True)
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+    values = (username, password)
     #print("This part is running now")
     #for item in values:
     #    print("Values are: " + item)
-    login_window.mainloop()
-    #sql = ("SELECT ID FROM user_table WHERE username=%s AND password=%s")
-    #cursor.execute(sql, values)
-    #connection.commit()
-    #myresult = cursor.fetchone()
-    #list = []
-    #if myresult is None:
-    #    print("Incorrect credentials. Try again!")
-    #    return [-1]
-    #for x in myresult:
-    #    if isinstance(x, int):
-    #        print("Logged in!")
-    #        list.append(x)
-    #        print(list)
-    #        return list
-    #cursor.close()
+    #login_window.mainloop()
+    sql = ("SELECT ID FROM user_table WHERE username=%s AND password=%s")
+    cursor.execute(sql, values)
+    connection.commit()
+    myresult = cursor.fetchone()
+    list = []
+    if myresult is None:
+        print("Incorrect credentials. Try again!")
+        return [-1]
+    for x in myresult:
+        if isinstance(x, int):
+            print("Logged in!")
+            list.append(x)
+            #print(list)
+            return list
+    cursor.close()
 
-# User checks their current balance expressed as a decimal in the form of 0.00.
+# User checks their current balance expressed as a decimal in the form of $0.00. User ID is needed
+# in order to verify which user is accessing which balance.
 # This function is currently working.
 def check_balance(user_id):
     cursor = connection.cursor(buffered=True)
     while user_id == [-1]:
         print("Whoops! Please log in first.")
         user_id = log_in()
-        continue
-    print("This is running now.")
+    #print("This is running now.")
     cursor = connection.cursor()
     value = user_id
     check_data = ("SELECT BALANCE FROM user_table WHERE id=%s")
@@ -112,21 +115,29 @@ def check_balance(user_id):
     connection.commit()
     cursor.close()
 
-# Using another table, meant to track deposits that the user makes. Automatically updates user balance.
+# Using another table*, meant to track deposits that the user makes. Automatically updates user balance.
 # May want to update function such that only data from the tables is being used rather than the pure user input.
 # Reason why transaction data is stored on another table is so the user can check their transaction history
 # in part to the usage of a foreign uid key.
 # This function is currently working. NOT USING FOREIGN KEY
+
+# * Currently the function is just adding to the balance column in the original user_table since I couldn't
+# figure out the foreign key, however I have an idea on how this would work
+# It would create a separate transaction ID that needs the user ID to know who the transaction belongs to.
+# Then the transaction would be labeled either as a deposit and withdrawl and the function would work
+# from that.
 def deposit(user_id):
     cursor = connection.cursor(buffered=True)
     while user_id == [-1]:
         print("Whoops! Please log in first.")
         user_id = log_in()
-    try:
-        amount = float(input("How much would you like to deposit? "))
-    except ValueError:
-        print("Whoops! Please enter a decimal number.")
-        amount = float(input("How much would you like to deposit? "))
+    amount = 0.00
+    while amount == 0.00:
+        try:
+            amount = float(input("How much would you like to deposit? "))
+        except ValueError:
+            print("Whoops! Please enter a decimal number.")
+
     value = user_id
 
     get_balance = ("SELECT BALANCE FROM user_table WHERE id=%s")
@@ -137,20 +148,24 @@ def deposit(user_id):
 
     newresult = smyresult + amount
     snewresult = str(newresult)
-    user_id = str(user_id[0])
+    user_id = user_id[0] # could be optimized
 
     query = ("UPDATE user_table SET BALANCE = %s WHERE id=%s")
     cursor.execute(query, (snewresult, user_id))
     
     connection.commit()
-    print(cursor.rowcount, "records changed.")
+    print(cursor.rowcount, "record changed.")
     cursor.close()
 
-# Using another table, meant to track withdrawals that the user makes. Automatically updates user balance.
+# Using another table*, meant to track withdrawals that the user makes. Automatically updates user balance.
 # May want to update function such that only data from the tables is being used rather than the pure user input.
 # Reason why transaction data is stored on another table is so the user can check their transaction history
 # in part to the usage of a foreign uid key.
-# This function is currently working. NOT USING FOREUGN KEY
+# This function is currently working. NOT USING FOREIGN KEY
+
+# * Currently the function is just subtracting from the balance column in the original user_table since I couldn't
+# figure out the foreign key, however I have an idea on how this would work. The implementation is similar to desposit
+# as listed above.
 def withdraw(user_id):
     cursor = connection.cursor(buffered=True)
     while user_id == [-1]:
@@ -175,13 +190,13 @@ def withdraw(user_id):
         return
 
     snewresult = str(newresult)
-    user_id = str(user_id[0])
+    user_id = user_id[0] # could be optimized
 
     query = ("UPDATE user_table SET BALANCE = %s WHERE id=%s")
     cursor.execute(query, (snewresult, user_id))
 
     connection.commit()
-    print(cursor.rowcount, "records changed.")
+    print(cursor.rowcount, "record changed.")
     cursor.close()
 
 # User enters in new data to update their existing account. User ID is needed to verify identity.
@@ -195,10 +210,8 @@ def modify_account(user_id):
     username = input("Create new username: ")
     password = input("Create new password: ")
     email = input("Enter your new email: ")
-    integer = -1
-    for item in user_id:
-        integer = item
-    values = (name, username, password, email, integer)
+    user_id = user_id[0] # could be optimized
+    values = (name, username, password, email, user_id)
     add_data = ("UPDATE user_table SET name = %s, username = %s, password = %s, email = %s WHERE ID = %s")
     cursor.execute(add_data, values)
     connection.commit()
